bc8426448e650248c335248491b0aa1dc8bf5708
ahmednaser111 <ahmednaser7707@gmail.com>
2022-12-14 14:50:35 +0200

    sign up\in activity added

--- app/src/main/java/com/example/dashboard/MainActivity.java
+++ app/src/main/java/com/example/dashboard/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.dashboard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Bundle bundle;
+    String name;
+    static String email;
+    TextView display_name;
+    ImageView log_out;
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = getApplicationContext();
+
+        bundle = getIntent().getExtras();
+        name = bundle.getString("name");
+        email = bundle.getString("email");
+
+        display_name = findViewById(R.id.display_name);
+        display_name.setText(name);
+
+        log_out = findViewById(R.id.log_out);
+        log_out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new lastSignedDB(context).logOut();
+                logOut();
+            }
+        });
+    }
+
+    public void logOut(){
+        finish();
+        startActivity(new Intent(context,signIn.class));
     }
 }
